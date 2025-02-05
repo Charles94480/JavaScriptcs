274a86f3779858878c219f3dacc6f742f022e193
Charles94480 <charlesmendonca11@gmail.com>
2025-02-05 19:30:27 -0300

    as

--- aula12/index.js
+++ aula12/index.js
@@ -1,9 +1,10 @@
-let idade = 17
-if (idade > 18){
+let idade = 91
+if (idade >= 18){
     console.log('vota')
 }
-else{
-   if(idade >= 16 && idade<18){
+else if(idade >= 16 && idade >= 65){
     console.log('voto opcional')
    }
+else if (idade <16){
+    console.log('não vota')
 }
