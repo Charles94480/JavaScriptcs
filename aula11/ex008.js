7c41d936c235d1617c3250fd57601f9f12945b18
Charles94480 <charlesmendonca11@gmail.com>
2025-02-05 12:16:09 -0300

    as

---
+++ aula11/ex008.js
@@ -0,0 +1,7 @@
+let vel = 30.5
+console.log(`A velocidade do seu codigo é ${vel}km/hr`)
+if(vel > 40){
+    console.log(`Você ultrapassou a velocidade permitida multado`)
+}
+
+console.log(`Dirija sempre com cinto de segurança`)
---
+++ aula11/ex009.js
@@ -0,0 +1,7 @@
+let pais = "brasil"
+if(pais == "brasil"){
+    console.log("seu pais é o brasil")
+}
+else{
+    console.log("seu pais não é o brasil")
+}
